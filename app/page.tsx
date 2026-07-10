db5b797ac02f18a7fbe93ee72a997041c5dfa3a8
Adarsh Singh Tomar <tomaradarsh18@gmail.com>
2026-07-10 09:46:26 +0530

    removing unused dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import Hero from "@/components/Hero";
 import InfoGrid from "@/components/InfoGrid";
-import Experience from "@/components/Experience";
 import Projects from "@/components/Projects";
-import Skills from "@/components/Skills";
 import Footer from "@/components/Footer";
 
 export default function Home() {
